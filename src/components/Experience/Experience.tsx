4a6d2db57e0e8442af7dc2c14a35f810a6768e1c
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-07-25 09:23:19 +0200

    add Skills and history in data json

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -5,26 +5,29 @@ import styles from "./Experience.module.css";
 
 export const Experience = () => {
     return (
-        <section id="experience">
-            <h2>Experience</h2>
-            <div>
-                <div>
+        <section className={styles.container} id="experience">
+            <h2 className={styles.title}>Experience</h2>
+            <div className={styles.content}>
+                <div className={styles.skills}>
                     {
                         skills.map((skill, id) => {
                             return (
-                                <div key={id}>
-                                    <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} className={styles.skillImg}/></div>
+                                <div key={id} className={styles.skill}>
+                                    <div className={styles.skillImageContainer}>
+                                        <img src={getImageUrl(skill.imageSrc)} alt={skill.title} className={styles.skillImg}/></div>
                                     <p>{skill.title}</p>
                                 </div>
                             );
                         })
                     }
                 </div>
-                <ul>{
+                <ul className={styles.history}>
+                    {
                     history.map((historyItem, id) => {
-                        return <li key={id}>
+                        return ( 
+                        <li key={id} className={styles.historyItem}>
                             <img src={getImageUrl(historyItem.imageSrc)} alt={historyItem.organisation} />
-                            <div>
+                            <div className={styles.historyItemDetails}>
                                 <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                                 <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                                 <ul>
@@ -35,7 +38,7 @@ export const Experience = () => {
                                 </ul>
                             </div>
                         </li>
-                    })
+                    )})
                 }</ul>
             </div>
         </section>

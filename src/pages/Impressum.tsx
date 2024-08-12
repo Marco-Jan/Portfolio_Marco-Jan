f04247b50635bb58944ab13cae36e999dbd8cc4a
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-08-12 17:50:31 +0200

    [FIX]fix impressumand datenschutzshow in vercel

---
+++ src/pages/Impressum.tsx
@@ -0,0 +1,33 @@
+
+
+function Impressum() {
+  return (
+    <div className="impressum-container">
+      <h1>Impressum</h1>
+      <p><strong>Marco Jan</strong><br />
+        Körösistraße 196<br />
+        8010 Graz<br />
+        Austria
+      </p>
+
+      <p><strong>E-Mail:</strong> <a href="mailto:marco.jan@gmx.de">marco.jan@gmx.de</a></p>
+
+      <p><strong>Verantwortlich für den Inhalt:</strong> Marco Jan</p>
+
+      <h2>Haftungsausschluss</h2>
+      <p>Trotz sorgfältiger inhaltlicher Kontrolle übernehme ich keine Haftung für die Inhalte externer Links.
+        Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+      </p>
+
+      <h2>EU-Streitschlichtung</h2>
+      <p>Gemäß der Verordnung über Online-Streitbeilegung in Verbraucherangelegenheiten möchte ich Sie über die
+        Online-Streitbeilegungs-Plattform (OS-Plattform) informieren. Verbraucher haben die Möglichkeit,
+        Beschwerden an die Online-Streitbeilegungs-Plattform der Europäischen Kommission unter
+        <a href="http://ec.europa.eu/odr" target="_blank" rel="noopener noreferrer">http://ec.europa.eu/odr</a> zu richten.
+        Die dafür notwendigen Kontaktdaten finden Sie oberhalb in meinem Impressum.
+      </p>
+    </div>
+  );
+}
+
+export default Impressum;
---
+++ src/pages/home.tsx
@@ -0,0 +1,38 @@
+
+import styles from "./home.module.css";
+import { Navbar } from "../components/Navbar/Navbar";
+import { Hero } from "../components/Hero/Hero";
+import { About } from "../components/About/About";
+import { Experience } from "../components/Experience/Experience"
+import { Projects } from "../components/Projects/Projects"
+import { Contact } from "../components/Contact/Contact";
+// import { AnimatedBackground } from "./components/AnimatedBackground/AnimatedBackground";
+import { SubFooter } from "../components/SubFooter/SubFooter";
+import { ScrollUpButton } from "../components/ScrollUpBtn/ScrollUpBtn";
+
+
+
+function App() {
+
+  
+
+  return <div className={styles.App}>
+    {/* <AnimatedBackground /> */}
+   
+    {/* <div className={styles.topOneBlur} /> */}
+    {/* <div className={styles.topTwoBlur} /> */}
+
+    <Navbar />
+    <Hero />
+    <About />
+    <Experience />
+    <Projects />
+    <ScrollUpButton />
+    <Contact />
+    <SubFooter />
+
+  </div>;
+
+}
+
+export default App
